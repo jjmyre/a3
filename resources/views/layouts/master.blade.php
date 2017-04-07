b598370fda4f9bb85020c297be923bef42121dc4
Justin Myre <justin.myre@gmail.com>
2017-04-06 17:18:26 -0700

    Fixed html validation error

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="{{ asset('favicon.ico') }}" rel="shortcut icon" />
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
-	<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/
-	bootstrap-combined.no-icons.min.css" rel="stylesheet" />
+	<link href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet" />
 	<link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel='stylesheet' />
     <link href="{{ asset('css/style.css') }}" type='text/css' rel='stylesheet' />
 	
